ba48572fd98eb844df5e2420b9bc8e0588928fd2
italo <italogithub@gmail.com>
2022-12-09 10:23:13 -0300

    update

--- Deb.py
+++ Deb.py
@@ -1,4 +1,19 @@
 def mostre_acerto(name_test):
-    print(f"{name_test}:\t Passou U+2705")
+    print(f"{name_test}:\t Passou ✅")
 def mostra_erro(name_test):
-    print(f"{name_test}:\t Não Passou U+274C")
+    print(f"{name_test}:\t Não Passou ❌")
+
+def multi_test(func):
+    # testa a função
+    if func(5, 8) == 5*6:
+        mostre_acerto(func.__name__)
+    else:
+        mostra_erro(func.__name__)
+
+def multi_recursi(num1, num2):
+    if num2 == 2:
+        return num1
+    else:
+        return num1 + multi_recursi(num1, num2 -1)
+
+multi_test(multi_recursi)
