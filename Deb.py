f37e3f46ac7febdca7e16cd717d42a01e59c12c4
italo <italogithub@gmail.com>
2022-12-09 10:24:32 -0300

    update

--- Deb.py
+++ Deb.py
@@ -5,13 +5,13 @@ def mostra_erro(name_test):
 
 def multi_test(func):
     # testa a função
-    if func(5, 8) == 5*6:
+    if func(5, 6) == 5*6:
         mostre_acerto(func.__name__)
     else:
         mostra_erro(func.__name__)
 
 def multi_recursi(num1, num2):
-    if num2 == 2:
+    if num2 == 1:
         return num1
     else:
         return num1 + multi_recursi(num1, num2 -1)
